e099cb91ea509566cf9c890623956540f706e47a
VasckMe <kasarin.anton@gmail.com>
2022-09-10 12:32:57 +0300

    Project Created

---
+++ Core Data App/Core Data App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Core Data App
+//
+//  Created by Apple Macbook Pro 13 on 10.09.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
